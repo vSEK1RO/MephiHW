ac439086b415a5661f38a9a0c28afdc2fade626a
vSEK1RO <vladlitvinov2006@gmail.com>
2024-01-22 23:19:42 +0300

    sem1

--- inf0/1.3.cpp
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-int main(){
-    float a=123.45, b=9.876, c=45.6;
-    printf("alpha\t=\t%.2f\nbeta\t=\t%.3f\ngamma\t=\t%.1f\n",a,b,c);
-}
--- inf0/1.5.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main(){
-    int a;
-    scanf("%d",&a);
-    if(a%2==0 and a>0){
-        printf("%d - Четное и положительное",a);
-    }
-}
